8d074f40fd4408f572d9cbde0ef17f1601429816
Sasa Jeftic <sasajeftic@outlook.com>
2017-11-01 14:54:05 +0100

    starter kitt

---
+++ template/footer.php
@@ -0,0 +1,9 @@
+
+  </div><!-- wrapper -->
+
+  <!-- Javascript -->
+  <script type="text/javascript" src="assets/js/jquery-2.1.4.min.js" />
+  <script type="text/javascript" src="assets/js/min/functions.min.js" />
+
+</body>
+</html>
